bf7ddeb498cc267936dffd403584ddc53c240bd9
ScannerWa <stoltzfusjay86@gmail.com>
2022-10-09 08:57:01 -0400

    Made some changes

--- src/book.py
+++ src/book.py
@@ -1,5 +1,3 @@
-
-
 class Book():
     def __init__(self, title='', author='', publisher='', formats=[], price=0,edition='',description='',bundle_title='', bundle_type=''):
         self.title = title
@@ -7,11 +5,14 @@ class Book():
         self.publisher = publisher
         self.formats = formats
         self.price = price
-        self.edition = edition
+
         self.bundle_title = bundle_title
         self.bundle_type = bundle_type
 
         self.description = description
+
+    
+    
 
     
     def __str__(self):
@@ -41,6 +42,9 @@ class Book():
         '''
 
 
+
+
 if __name__ == '__main__':
     book = Book(title="Test",bundle='The Ultimate Bundle', author='Jay Stolt', publisher='Print House', formats=['PDF', 'EPUB', 'MOBI'], price=18, edition='1st Edition', description='This is for testing purposes')
     print(book)
+    
