97e09527905083e4c5462db8ba55516adf6824dd
ScannerWa <stoltzfusjay86@gmail.com>
2022-10-11 10:26:13 -0400

    Made changes

--- src/api.py
+++ src/api.py
@@ -1,7 +1,6 @@
-from urllib import request
 from flask import Flask
 from scrape import Scraper
-from bundle_page import MainPage, BundlePages
+
 app = Flask(__name__)
 
     
@@ -11,14 +10,14 @@ def get_current_bundles():
     bundles = scrape_bundles()
 
     return bundles 
+
+@app.get('/user')
+def new_user():
+    pass
     
 def scrape_bundles():
-    scraper = Scraper(url='https://www.humblebundle.com/books/')
-    main_page = MainPage(scraper.main_html)
-#    bundles = []
-    scraper.scrape_bundle_pages(main_page.bundle_urls)
-    bundle_pages = scraper.bundle_htmls
-    bundles = BundlePages(bundle_pages)
-    return bundles.bundles
 
-scrape_bundles()
+    scrape = Scraper()
+    scrape.scrape_bundle_pages()
+    scrape.parse_bundle_pages()
+    return scrape.bundles
